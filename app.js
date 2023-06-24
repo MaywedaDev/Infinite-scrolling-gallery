de318253e68d4070923ff3da4173659de8cfd502
Enyo <enyoonuche1@gmail.com>
2023-06-24 17:48:07 +0100

    Updated code

--- app.js
+++ app.js
@@ -1,3 +1,91 @@
 import { status } from "./util.js"
+import { calcHeightAndWidth } from "./util.js"
+
+
+var next_page = 'https://api.pexels.com/v1/curated/?page=&per_page=15'
+
+const createImages = async () => {
+
+    const pexelApiRequest = await fetch(next_page, {
+        method: "GET",
+        headers: {
+            Authorization: "sPJBecVTFHoskjEhAhPWQnblRXHdnPeUHL5FYNGTv1OY33hmwXaUZ64n"
+        }
+    })
+
+    const response = await pexelApiRequest.json()
+
+    console.log(response)
+
+    generateImages(response)
+}
+
+const generateImages = (data) => {
+    const imageCont = document.getElementById("images")
+
+
+    data.photos.forEach((el) => {
+        imageCont.appendChild(createSingleImage(el))
+    })
+
+    next_page = data.next_page
+    console.log(next_page)
+}
+
+const createSingleImage = (el) => {
+    const div = document.createElement('div')
+    const img = document.createElement("img")
+    const a = document.createElement('a')
+    const creds = createCredits(el.photographer, el.photographer_url)
+
+    a.href = el.url
+    a.classList.add('topexels')
+
+    div.classList.add('imgbox')
+    div.style.gridRow = `span ${ calcHeightAndWidth(el.width, el.height) }`
+
+    img.alt = el.alt
+    img.src = el.src.medium
+    img.classList.add('w-100')
+
+
+    div.appendChild(a)
+    div.appendChild(creds)
+    div.appendChild(img)
+
+    return div
+}
+
+const createCredits = (name, url) => {
+    const div = document.createElement('div')
+    const p = document.createElement('p')
+    const a = document.createElement('a')
+    const made_by = document.createElement('span')
+
+    p.innerText = name
+    a.href = url
+    a.classList.add('tophotographer')
+    a.innerText = "Go to Photographer"
+    made_by.innerText = "Made by"
+
+    div.classList.add('credits')
+    div.appendChild(made_by)
+    div.appendChild(p)
+    div.appendChild(a)
+
+    return div
+}
+
+const getMoreImages = () => {
+    if(window.scrollY >= (document.documentElement.offsetHeight - window.innerHeight)){
+        console.log('reached bottom')
+        createImages();
+    }      
+}
+
+window.addEventListener('scroll', getMoreImages)
 
 console.log(status)
+
+createImages();
+
--- util.js
+++ util.js
@@ -1,1 +1,10 @@
 export const status = 'It works?'
+
+export const calcHeightAndWidth = (width, height) => {
+    const compressionRatio = Math.round(width / 250)
+    const calculatedHeight = height / compressionRatio
+
+    const rowSpan = Math.floor(calculatedHeight / 10)
+
+    return rowSpan
+}
